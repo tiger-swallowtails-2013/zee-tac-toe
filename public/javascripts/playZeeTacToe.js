7211b19db78714d83c5fad9ef51697426b243d36
Zee Spencer <zee@zeespencer.com>
2013-09-26 12:35:45 -0700

    Squares have classes for player_z and _x when clicked

--- public/javascripts/playZeeTacToe.js
+++ public/javascripts/playZeeTacToe.js
@@ -1,8 +1,20 @@
-function playZeeTacToe(eventListeners) {
+function playZeeTacToe() {
+  var clickCount = 0;
   var gameBoardElement = document.getElementById('zee_tac_toe');
   showElement(gameBoardElement);
+  var squares = document.getElementsByClassName('square');
+
+  squares[0].addEventListener('click', function() {
+    var playerClass = clickCount == 0 ? 'player_x' : 'player_z'
+    addClass(squares[0], playerClass);
+    clickCount++;
+  });
+
 }
 
+function addClass(element, cssClass) {
+  element.classList.add(cssClass);
+}
 function showElement(element) {
   element.classList.remove('hidden');
 }
--- spec/javascripts/playZeeTacToeSpec.js
+++ spec/javascripts/playZeeTacToeSpec.js
@@ -1,20 +1,43 @@
 describe("playZeeTacToe", function() {
-  it("shows the #zee_tac_toe div", function() {
-    var zeeTacToeDiv = document.createElement('div');
+  var zeeTacToeDiv, square;
+  beforeEach(function() {
+    zeeTacToeDiv = document.createElement('div');
     zeeTacToeDiv.classList.add('hidden');
     zeeTacToeDiv.id='zee_tac_toe';
 
+    square = document.createElement('div');
+    square.classList.add('square');
+    zeeTacToeDiv.appendChild(square);
+
     document.body.appendChild(zeeTacToeDiv);
 
-    playZeeTacToe();
-
+  });
+  afterEach(function() {
     document.getElementById('zee_tac_toe').remove();
+  });
+
+  it("shows the #zee_tac_toe div", function() {
+    playZeeTacToe();
 
     expect(zeeTacToeDiv.classList.contains("hidden")).toBeFalsy();
   });
 
+  it('marks square with "X" when clicked for the first time', function() {
+    playZeeTacToe();
 
-  xit("binds click handlers to the squares", function() {
+    square.dispatchEvent(new Event('click'));
+
+    expect(square.classList.contains('player_x')).toBeTruthy();
+  });
+
+  it('marks square with "Z" when clicked for the second time', function() {
+    playZeeTacToe();
+
+    square.dispatchEvent(new Event('click'));
+    square.dispatchEvent(new Event('click'));
+
+    expect(square.classList.contains('player_z')).toBeTruthy();
+
   });
 
 });
